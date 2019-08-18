f5e985fae5df12db3bdd0557b4a5b0db92eb7e05
clark-lindsay <clarklindsay96@gmail.com>
2019-08-18 10:31:32 -0400

    formatted the pencil and penciltest files

--- Pencil.js
+++ Pencil.js
@@ -1,117 +1,114 @@
 class Pencil {
-    constructor( pointDurability = 100, length = 7, eraserDurability = 50 ) {
-        this.page = ``;
-        [this.pointDurability, this.initialDurability] = [pointDurability, pointDurability];
-        this.length = length;
-        this.eraserDurability = eraserDurability;
-        this.erasureIndices = [];
+    constructor(pointDurability = 100, length = 7, eraserDurability = 50) {
+      this.page = ``;
+      [this.pointDurability, this.initialDurability] = [pointDurability, pointDurability];
+      this.length = length;
+      this.eraserDurability = eraserDurability;
+      this.erasureIndices = [];
     }
-
+  
     write(text) {
-        for (const char of text) {
-            const charValue = valueOfCharacter(char);
-
-            if (this.pointDurability > 0) {
-                if (charValue === 2) {
-                    this.pointDurability -= charValue;
-                    if (this.pointDurability >= 2) {
-                        this.page += char;
-                    }
-                    else {
-                        this.page += ' '
-                    }
-                }
-                else if (charValue === 1) {
-                    this.pointDurability -= charValue;
-                    this.page += char;
-                }
-                else {
-                    this.page += char;
-                }
+      for (const char of text) {
+        const charValue = valueOfCharacter(char);
+  
+        if (this.pointDurability > 0) {
+          if (charValue === 2) {
+            this.pointDurability -= charValue;
+            if (this.pointDurability >= 2) {
+              this.page += char;
+            } else {
+              this.page += ' ';
             }
-            else {
-                this.page += ' ';
-            }
+          } else if (charValue === 1) {
+            this.pointDurability -= charValue;
+            this.page += char;
+          } else {
+            this.page += char;
+          }
+        } else {
+          this.page += ' ';
         }
+      }
     }
-
+  
     sharpen() {
-        if (this.length > 0) {
-            this.pointDurability = this.initialDurability;
-            this.length -= 1;
-        }
+      if (this.length > 0) {
+        this.pointDurability = this.initialDurability;
+        this.length -= 1;
+      }
     }
-
+  
     erase(text) {
-       if (this.page.includes(text) && this.eraserDurability > 0) {
-           const wordLength = text.length; 
-           const numCharsToErase = Math.min(wordLength, this.eraserDurability);
-           const replacementText = text.slice(0, wordLength - numCharsToErase) + ' '.repeat(numCharsToErase);
-           const lastOccurenceOfText = this.page.lastIndexOf(text);
-
-           const newTrailingEnd = this.page.slice(lastOccurenceOfText).replace(text, replacementText);
-
-           this.page = this.page.slice(0, lastOccurenceOfText) + newTrailingEnd;
-
-           this.eraserDurability -= numCharsToErase;
-           this.erasureIndices.push(lastOccurenceOfText);
-       }
+      if (this.page.includes(text) && this.eraserDurability > 0) {
+        const wordLength = text.length;
+        const numCharsToErase = Math.min(wordLength, this.eraserDurability);
+        const replacementText =
+          text.slice(0, wordLength - numCharsToErase) + ' '.repeat(numCharsToErase);
+        const lastOccurenceOfText = this.page.lastIndexOf(text);
+  
+        const newTrailingEnd = this.page.slice(lastOccurenceOfText).replace(text, replacementText);
+  
+        this.page = this.page.slice(0, lastOccurenceOfText) + newTrailingEnd;
+  
+        this.eraserDurability -= numCharsToErase;
+        this.erasureIndices.push(lastOccurenceOfText);
+      }
     }
-
+  
     edit(textToInsert) {
+      const writeEdit = () => {
         const lastErasureIndex = this.erasureIndices[this.erasureIndices.length - 1];
-        const writeEdit = () => {
-            let editedText = '';
-            for (let i = 0; i < textToInsert.length; ++i) {
-                if (this.pointDurability <= 0) {
-                    break;
-                }
-
-                if ((i + lastErasureIndex) < this.page.length && isNotWhitespace(this.page[i + lastErasureIndex])) {
-                    editedText += '@';
-                    this.pointDurability -= 1;
-                }
-                else {
-                    const charValue = valueOfCharacter(textToInsert[i]);
-                    if (this.pointDurability >= charValue) {
-                        editedText += textToInsert[i];
-                        this.pointDurability -= charValue;
-                    }
-                    else {
-                        break;
-                    }
-                }
+        let editedText = '';
+  
+        for (let i = 0; i < textToInsert.length; ++i) {
+          if (this.pointDurability <= 0) {
+            break;
+          }
+  
+          if (
+            i + lastErasureIndex < this.page.length &&
+            isNotWhitespace(this.page[i + lastErasureIndex])
+          ) {
+            editedText += '@';
+            this.pointDurability -= 1;
+          } else {
+            const charValue = valueOfCharacter(textToInsert[i]);
+            if (this.pointDurability >= charValue) {
+              editedText += textToInsert[i];
+              this.pointDurability -= charValue;
+            } else {
+              break;
             }
-            return editedText;
+          }
         }
-
-        if (this.erasureIndices.length !== 0 && this.pointDurability > 0) {
-            const newPageSection = this.page.slice(0, lastErasureIndex) + writeEdit();
-            
-            this.page = newPageSection + this.page.slice(newPageSection.length);
-            this.erasureIndices.pop();
-        }
+        return this.page.slice(0, lastErasureIndex) + editedText;
+      };
+  
+      if (this.erasureIndices.length !== 0 && this.pointDurability > 0) {
+        const newPageSection = writeEdit();
+  
+        this.page = newPageSection + this.page.slice(newPageSection.length);
+        this.erasureIndices.pop();
+      }
     }
-}
-
-function valueOfCharacter(char) {
+  }
+  
+  function valueOfCharacter(char) {
     if (isACapitalLetter(char)) {
-        return 2;
+      return 2;
+    } else if (isNotWhitespace(char)) {
+      return 1;
     }
-    else if (isNotWhitespace(char)) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
-}
-
-function isACapitalLetter(char) {
+    return 0;
+  }
+  
+  function isACapitalLetter(char) {
     return char.match(/[A-Z]/);
-}
-
-function isNotWhitespace(char) {
-    return char.match(/\S/); 
-}
-
-module.exports = Pencil;
+  }
+  
+  function isNotWhitespace(char) {
+    return char.match(/\S/);
+  }
+  
+  module.exports = Pencil;
+  
--- Pencil.test.js
+++ Pencil.test.js
@@ -1,164 +1,168 @@
 const Pencil = require('./Pencil');
 
 describe('Pencil', () => {
-    test('it can write a single word', () => {
-        pencil = new Pencil();
-        pencil.write('fudge');
+  test('it can write a single word', () => {
+    pencil = new Pencil();
+    pencil.write('fudge');
 
-        expect(pencil.page).toEqual('fudge');
-    });
+    expect(pencil.page).toEqual('fudge');
+  });
 
-    test('when writing more than once, subsequent writes are appended to the existing text', () => {
-        pencil = new Pencil();
-        pencil.write('She sells sea shells');
-        pencil.write(' down by the sea shore');
+  test('when writing more than once, subsequent writes are appended to the existing text', () => {
+    pencil = new Pencil();
+    pencil.write('She sells sea shells');
+    pencil.write(' down by the sea shore');
 
-        expect(pencil.page).toEqual('She sells sea shells down by the sea shore');
-    });
+    expect(pencil.page).toEqual('She sells sea shells down by the sea shore');
+  });
 
-    test('after writing too many non-whitespace characters, the pencil only produces space characters', () => {
-        pencil = new Pencil(4);
-        pencil.write('Text');
-        
-        secondPencil = new Pencil(5);
-        secondPencil.write('   \nspaces')
+  test('after writing too many non-whitespace characters, the pencil only produces space characters', () => {
+    pencil = new Pencil(4);
+    pencil.write('Text');
 
-        expect(pencil.page).toEqual('Tex ');
-        expect(secondPencil.page).toEqual('   \nspace ');
-    });
+    secondPencil = new Pencil(5);
+    secondPencil.write('   \nspaces');
 
-    test('it will write a space if tasked to write a capital when it has only one durability left', () => {
-        pencil = new Pencil(4);
-        pencil.write('TnT is dynamite');
-        
-        expect(pencil.page).toEqual('Tn             ');
-    });
+    expect(pencil.page).toEqual('Tex ');
+    expect(secondPencil.page).toEqual('   \nspace ');
+  });
 
-    test('it can be sharpened to regain durability', () => {
-        pencil = new Pencil(4);
-        pencil.write('Text');
-        pencil.sharpen();
-        pencil.write('word');
+  test('it will write a space if tasked to write a capital when it has only one durability left', () => {
+    pencil = new Pencil(4);
+    pencil.write('TnT is dynamite');
 
-        secondPencil = new Pencil(40000);
-        secondPencil.write('A somewhat long sentence');
-        secondPencil.sharpen();
+    expect(pencil.page).toEqual('Tn             ');
+  });
 
-        expect(pencil.page).toEqual('Tex word');
-        expect(secondPencil.pointDurability).toEqual(40000);
-    });
+  test('it can be sharpened to regain durability', () => {
+    pencil = new Pencil(4);
+    pencil.write('Text');
+    pencil.sharpen();
+    pencil.write('word');
 
-    test('if it runs out of length, it cannot regain durability', () => {
-        pencil = new Pencil(4, 2);
-        pencil.write('word');
-        pencil.sharpen();
-        pencil.write('word');
-        pencil.sharpen();
-        pencil.write('word');
-        pencil.sharpen();
-        pencil.write('word');
+    secondPencil = new Pencil(40000);
+    secondPencil.write('A somewhat long sentence');
+    secondPencil.sharpen();
 
-        expect(pencil.page).toEqual('wordwordword    ');
+    expect(pencil.page).toEqual('Tex word');
+    expect(secondPencil.pointDurability).toEqual(40000);
+  });
 
-        pencil.sharpen();
-        expect(pencil.pointDurability).toEqual(0);
-    });
+  test('if it runs out of length, it cannot regain durability', () => {
+    pencil = new Pencil(4, 2);
+    pencil.write('word');
+    pencil.sharpen();
+    pencil.write('word');
+    pencil.sharpen();
+    pencil.write('word');
+    pencil.sharpen();
+    pencil.write('word');
 
-    test('it can erase previously written text', () => {
-        pencil = new Pencil();
-        pencil.write('How much wood would a woodchuck chuck if a woodchuck could chuck wood');
-        pencil.erase('chuck');
+    expect(pencil.page).toEqual('wordwordword    ');
 
-        expect(pencil.page).toEqual('How much wood would a woodchuck chuck if a woodchuck could       wood')
+    pencil.sharpen();
+    expect(pencil.pointDurability).toEqual(0);
+  });
 
-        pencil.erase('chuck');
+  test('it can erase previously written text', () => {
+    pencil = new Pencil();
+    pencil.write('How much wood would a woodchuck chuck if a woodchuck could chuck wood');
+    pencil.erase('chuck');
 
-        expect(pencil.page).toEqual('How much wood would a woodchuck chuck if a wood      could       wood')
-    });
+    expect(pencil.page).toEqual(
+      'How much wood would a woodchuck chuck if a woodchuck could       wood',
+    );
 
-    test('it will not erase something that is not there', () => {
-        pencil = new Pencil();
-        pencil.write('Some normal words');
-        pencil.erase('chuck');
+    pencil.erase('chuck');
 
-        expect(pencil.page).toEqual('Some normal words');
+    expect(pencil.page).toEqual(
+      'How much wood would a woodchuck chuck if a wood      could       wood',
+    );
+  });
 
-        pencil.erase('mornal');
-        pencil.erase('normale');
+  test('it will not erase something that is not there', () => {
+    pencil = new Pencil();
+    pencil.write('Some normal words');
+    pencil.erase('chuck');
 
-        expect(pencil.page).toEqual('Some normal words');
-    });
+    expect(pencil.page).toEqual('Some normal words');
 
-    test('it will do nothing when erase is called when nothing has yet been written', () => {
-        pencil = new Pencil();
-        pencil.erase('chuck');
+    pencil.erase('mornal');
+    pencil.erase('normale');
 
-        expect(pencil.page).toEqual('');
-    });
+    expect(pencil.page).toEqual('Some normal words');
+  });
 
-    test('it will eventually run out of eraser, and it erases the most recently written text first', () => {
-        pencil = new Pencil(100, 7, 3);
-        pencil.write('Buffalo Bill');
-        pencil.erase('Bill');
+  test('it will do nothing when erase is called when nothing has yet been written', () => {
+    pencil = new Pencil();
+    pencil.erase('chuck');
 
-        expect(pencil.page).toEqual('Buffalo B   ');
-        
-        pencil.erase('Buffalo');
+    expect(pencil.page).toEqual('');
+  });
 
-        expect(pencil.page).toEqual('Buffalo B   ');
-    });
+  test('it will eventually run out of eraser, and it erases the most recently written text first', () => {
+    pencil = new Pencil(100, 7, 3);
+    pencil.write('Buffalo Bill');
+    pencil.erase('Bill');
 
-    test('it can edit existing text, provided that some text has been erased', () => {
-        pencil = new Pencil();
-        pencil.write('An apple a day keeps the doctor away');
-        pencil.erase('apple');
-        pencil.edit('onion');
+    expect(pencil.page).toEqual('Buffalo B   ');
 
-        expect(pencil.page).toEqual('An onion a day keeps the doctor away');
-    });
+    pencil.erase('Buffalo');
 
-    test('it will use the @ character to mark overlap when editing text', () => {
-        pencil = new Pencil();
-        pencil.write('An apple a day keeps the doctor away');
-        pencil.erase('apple');
-        pencil.edit('artichoke');
+    expect(pencil.page).toEqual('Buffalo B   ');
+  });
 
-        expect(pencil.page).toEqual('An artich@k@ay keeps the doctor away');
-    });
+  test('it can edit existing text, provided that some text has been erased', () => {
+    pencil = new Pencil();
+    pencil.write('An apple a day keeps the doctor away');
+    pencil.erase('apple');
+    pencil.edit('onion');
 
-    test('it will stop writing or overwriting text if it is out of point durability during an edit', () => {
-        pencil = new Pencil(34);
-        pencil.write('An apple a day keeps the doctor away');
-        pencil.erase('apple');
-        pencil.edit('artichoke');
+    expect(pencil.page).toEqual('An onion a day keeps the doctor away');
+  });
 
-        expect(pencil.page).toEqual('An arti  a day keeps the doctor away');
-    });
+  test('it will use the @ character to mark overlap when editing text', () => {
+    pencil = new Pencil();
+    pencil.write('An apple a day keeps the doctor away');
+    pencil.erase('apple');
+    pencil.edit('artichoke');
 
-    test('it will not make an edit when there have been no erasures', () => {
-        pencil = new Pencil();
-        pencil.write('An apple a day keeps the doctor away');
-        pencil.edit('artichoke');
+    expect(pencil.page).toEqual('An artich@k@ay keeps the doctor away');
+  });
 
-        expect(pencil.page).toEqual('An apple a day keeps the doctor away');
-    });
+  test('it will stop writing or overwriting text if it is out of point durability during an edit', () => {
+    pencil = new Pencil(34);
+    pencil.write('An apple a day keeps the doctor away');
+    pencil.erase('apple');
+    pencil.edit('artichoke');
 
-    test('it will add words to the end of the page for an edit, if the last erasure was the last word on the page', () => {
-        pencil = new Pencil();
-        pencil.write('An apple a day keeps the doctor away');
-        pencil.erase('away')
-        pencil.edit('artichoke');
+    expect(pencil.page).toEqual('An arti  a day keeps the doctor away');
+  });
 
-        expect(pencil.page).toEqual('An apple a day keeps the doctor artichoke');
-    });
+  test('it will not make an edit when there have been no erasures', () => {
+    pencil = new Pencil();
+    pencil.write('An apple a day keeps the doctor away');
+    pencil.edit('artichoke');
 
-    test('will not make an edit if the number of edit requests is more than the number of erasures', () => {
-        pencil = new Pencil();
-        pencil.write('An apple a day keeps the doctor away');
-        pencil.erase('apple');
-        pencil.edit('artichoke');
-        pencil.edit('onion');
+    expect(pencil.page).toEqual('An apple a day keeps the doctor away');
+  });
 
-        expect(pencil.page).toEqual('An artich@k@ay keeps the doctor away');
-    });
+  test('it will add words to the end of the page for an edit, if the last erasure was the last word on the page', () => {
+    pencil = new Pencil();
+    pencil.write('An apple a day keeps the doctor away');
+    pencil.erase('away');
+    pencil.edit('artichoke');
+
+    expect(pencil.page).toEqual('An apple a day keeps the doctor artichoke');
+  });
+
+  test('will not make an edit if the number of edit requests is more than the number of erasures', () => {
+    pencil = new Pencil();
+    pencil.write('An apple a day keeps the doctor away');
+    pencil.erase('apple');
+    pencil.edit('artichoke');
+    pencil.edit('onion');
+
+    expect(pencil.page).toEqual('An artich@k@ay keeps the doctor away');
+  });
 });
